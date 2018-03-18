88f02e2f2ed2e3240457c39f0592963ab1600ea3
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-03-18 17:35:32 -0300

    dsv-evento Ajustado organização das divs principal e conteudo-topico

--- evento/view/cabecalho.php
+++ evento/view/cabecalho.php
@@ -3,5 +3,5 @@
     $data="xx/xx/xxxx";
 ?>
 <div class="img-cabecalho">
-<img id="img-cabecalho" src="../img/img-evento.png" alt="imagem" name="img-evento">
+    <img id="img-cabecalho" src="../img/img-evento.png" alt="imagem" name="img-evento">
 </div>
