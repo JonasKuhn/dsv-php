322847baa604824b34aa6d10f4f331544e8a072b
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-03-18 18:02:54 -0300

    dsv-evento Adicionado nova fonte para os tópicos

--- evento/view/inicial.php
+++ evento/view/inicial.php
@@ -2,4 +2,9 @@
     <hr>
     <h2><?php echo "$nomeEvento" ?></h2>
     <p class="evento_item"> O que é? </p>
+    
+    &emsp;Funcionamento da memória 
+    
+    <p>O sistema operacional é responsável por fazer o gerenciamento da memória RAM, 
+        ou seja, ele mantêm as partes principais do programa na memória.</p>
 </div>
