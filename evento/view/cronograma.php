2d813ef0a9b7bb99a13adb88293d5928ae1d7e19
Emerson <emersonklunk2014@gmail.com>
2018-03-20 20:08:43 -0300

    dsv-evento alteracoes feitas em aula

--- evento/view/cronograma.php
+++ evento/view/cronograma.php
@@ -15,5 +15,21 @@
         <p class="p-center">Time F X Time L</p>
 
         <p>Após isso sera feita as próximas tabelas de disputas.</p>
+        
+        <!--FEITO EM AULA -->
+        <p>
+        <label class="rotulo">Data:</label> 20/03/2018 <br>
+        <label class="rotulo">Local: </label><?php echo "$local" ?><br>
+        <label class="rotulo">Horário: </label> 22:56 <br>
+        <label class="rotulo">Atração: </label>Corrida de porco <br>
+        <label class="rotulo">Obs: </label> Só porco de raça <br>
+        </p><hr>
+        <p>
+        <label class="rotulo">Data:</label> 21/03/2018 <br>
+        <label class="rotulo">Local: </label><?php echo "$local" ?><br>
+        <label class="rotulo">Horário: </label> 12:00 <br>
+        <label class="rotulo">Atração: </label>Corrida de velha manca <br>
+        <label class="rotulo">Obs: </label>Todas de muleta <br>
+        </p>
     </div>
 </div>
