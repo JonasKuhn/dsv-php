15179876004253437558f98c96bea52dac0f5509
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-03-20 21:35:29 -0300

    dsv-evento Criado conexão com o banco

---
+++ evento/conexao/conexao.php
@@ -0,0 +1,23 @@
+<?php
+//PARAMETRODE DE CONFIGURAÇÃO INICIAL
+
+$host='localhost';
+$base='evento';
+$usuario='root';
+$senha='1234';
+
+//CONEXÃO AO BANCO DE DADOS
+
+$mysqli = new mysqli($host,$usuario,$senha,$base);
+
+//CASO TENHA ERRO 
+if(mysqli_connect_errno()){
+    trigger_error(mysqli_connect_error());
+}
+
+//ALTERA CARACTERES PARA UTF8
+$mysqli->set_charset("utf8");
+
+//Modifica a zona de horario para sp
+date_default_timezone_set('America/Sao_Paulo');
+
