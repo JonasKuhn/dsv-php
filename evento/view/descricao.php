1243917cec43e10775c4d9f0eb92232e1a50ace9
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-03-27 20:53:14 -0300

    dsv-evento Alterado chamada de tabela das informações

--- evento/view/descricao.php
+++ evento/view/descricao.php
@@ -1,17 +1,19 @@
 <?php
 include ('../conexao/conexao.php');
 
-$sql = "select nome_evento,data_inicio,data_fim"
-        . " from inicial";
+$sql = "select nome_evento,data,descricao_evento,valor_inscricao"
+        . " from tb_informacao";
+
 //mandando para o banco de dadoso comando de $sql//
 $query = $mysqli->query($sql);
 
 $dados = $query->fetch_array();
 $nomeEvento = $dados['nome_evento'];
-$dataInicio = $dados['data_inicio'];
-$dataFim = $dados['data_fim'];
+$data = $dados['data'];
+$descricao_evento = $dados['descricao_evento'];
+$valor_inscricao = $dados['valor_inscricao'];
 
-$dt_separacao= explode("-",$dados['data_inicio']);
+$dt_separacao= explode("-",$dados['data']);
 switch ($dt_separacao[1]) {
     case 1:$dt = "Janeiro"; break;
     case 2:$dt = "Fevereiro"; break;
@@ -26,7 +28,7 @@ switch ($dt_separacao[1]) {
     case 11:$dt = "Novembro"; break;
     case 12: $dt = "Dezembro"; break;
 }
-$data=$dt_separacao[2]." de ".$dt." de ".$dt_separacao[0];
+$data_inicio=$dt_separacao[2]." de ".$dt." de ".$dt_separacao[0];
 
 ?>
 <div class="conteudo">
@@ -34,14 +36,9 @@ $data=$dt_separacao[2]." de ".$dt." de ".$dt_separacao[0];
     <h1><?php echo "$nomeEvento" ?><br></h1>
     <div class="data">
         <img src="../img/icon-calendario.jpg" alt="icon-calendario" name="Calendario"> 
-        <span><?php echo "$data" ?></span>
+        <span><?= $data_inicio ?></span>
     </div>
     <div class="topico">
-        <p class="evento-item">&emsp;DESCRIÇÃO DO EVENTO</p>  
-        <p>&emsp;O 1º torneio de Vôlei de areia é um evento que foi motivado e organizado pelo
-            CAGETI (Centro Acadêmico da Gestão da Tecnologia da Informação) junto da coordenação do curso.</p>
-        <p>&emsp;Esse evento possuí o intuito de reunir atletas do próprio curso além de todo o pessoal que tiver interesse. </p>
-        <p>&emsp;Será um evento para motivar as pessoas a realizarem atividades físicas e também trabalhar a relação de trabalho em equipe.</p>
-        <p>&emsp;Serão compoetições entre duplas que por fim tera uma premiação para as tres duplas vencedoras.</p>
+        <span><p>&emsp;<?= $descricao_evento?></p></span>
     </div>
 </div>
