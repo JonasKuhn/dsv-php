2eaf5785bfcd33f1052f17f49ab52e09c7e5320f
Emerson <emersonklunk2014@gmail.com>
2018-03-27 20:52:47 -0300

    dsv-evento alteracao na forma de buscar os dados dos patrocinadores

--- evento/view/patrocinadores.php
+++ evento/view/patrocinadores.php
@@ -2,11 +2,23 @@
     <hr>
     <div class="patrocinadores">
         <div class="topico">
-            <p class="evento-item">&emsp;PATROCINADORES</p>    
-            <p>&emsp;UCEFF Itapiranga</p>
-            <p>Unidade Central de Educação FAI Faculdades - Centro Universitario FAI Itapiranga</p>
-            <p>&emsp;CBV</p>
-            <p>Confederação Brasileira de Voleibol</p>
+            <p class="evento-item">&emsp;PATROCINADORES</p> 
+            <?php
+            include ('../conexao/conexao.php');
+
+            $sql = "select * from tb_patrocinadores";
+//mandando para o banco de dadoso comando de $sql//
+            $query = $mysqli->query($sql);
+
+            while($dados = $query->fetch_array()){
+            $siglaPatrocinador = $dados['sigla_patrocinadores'];
+            $nomePatrocinador = $dados['nome_patrocinadores'];
+            ?>
+            
+               
+            <p>&emsp;<?php echo "$siglaPatrocinador"?></p>
+            <p><?php echo "$nomePatrocinador"?></p>
+            <?php } ?>
         </div>
     </div>
 </div>
