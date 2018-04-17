56b0a9f46afc76c57b59ab5d0f6f12c893ec17b9
Jonas Kuhn <jonaskuhn220@gmail.com>
2018-04-17 14:15:04 -0300

    dsv-php Ajustado name das imagens

--- evento/view/rodape.php
+++ evento/view/rodape.php
@@ -1,13 +1,13 @@
 <footer class="contatos">
     <div class="icon">
-        <a href="#"><img src="../img/icon-email.png" alt="e-mail"> </a>
+        <a><img src="../img/icon-email.png" alt="e-mail"> </a>
         <p>cageti@faifaculdades.edu.br</p>
     </div>
 
     <div class="break"></div>
     
     <div class="icon">
-        <a href="#"><img src="../img/fb.png" alt="facebook"> </a>
+        <a><img src="../img/fb.png" alt="facebook"> </a>
         <p>www.facebook.com/cageti</p>
     </div>
 
